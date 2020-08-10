04438af3bb1f1ff6ea46199b8ee3c3e0c5b57d23
jbreitzman <jbreitzman@gmail.com>
2020-08-10 13:54:17 -0400

    Configured the Application to use JWT for Spring Security.

---
+++ todo-app_Backend/src/main/java/io/breitek/SpringDemoApp/rest/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package io.breitek.SpringDemoApp.rest.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
